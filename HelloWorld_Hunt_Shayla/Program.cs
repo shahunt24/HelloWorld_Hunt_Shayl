1f2dd69271d67a76c5ce5ff07cf0ed534aaefb0c
Shayla <Shaylahunt834@gmail.com>
2025-02-25 15:02:01 -0600

    Add project files.

---
+++ HelloWorld_Hunt_Shayla/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld_Hunt_Shayla
+{
+    internal class Program
+    {
+        static void Main(string[] args) // The Main method is
+        {
+            Console.WriteLine("What's your first name?"); // Asks user for their first name
+            string firstName = Console.ReadLine(); // Assigns the user input firstName variable
+            Console.WriteLine("What is your last name?"); // Askes user for their last name
+            string lastName = Console.ReadLine(); // Assigns user input lastName variable
+
+            Console.WriteLine($"Hello, {firstName} {lastName}!"); // Prints a message that says first and last name
+        }
+    }
+}
